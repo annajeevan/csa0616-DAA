bc6dca4e60a6e15a47b76fe671233482b60b0b6c
annajeevan <annajeevan22@gmail.com>
2026-08-31 09:19:19 +0530

    Create 18_p_np_notes.cpp

---
+++ 18_p_np_notes.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "P:\n";
+    cout << "Problems that can be solved in polynomial time.\n\n";
+
+    cout << "NP:\n";
+    cout << "Problems whose solutions can be verified in polynomial time.\n\n";
+
+    cout << "NP-Hard:\n";
+    cout << "Problems at least as hard as every problem in NP.\n\n";
+
+    cout << "NP-Complete:\n";
+    cout << "Problems that are both NP and NP-Hard.\n\n";
+
+    cout << "Examples of NP-Complete problems:\n";
+    cout << "1. SAT\n";
+    cout << "2. 3-SAT\n";
+    cout << "3. Vertex Cover\n";
+    cout << "4. Hamiltonian Cycle\n";
+
+    return 0;
+}
