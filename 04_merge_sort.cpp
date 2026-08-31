8d931f811e0bc1b360888f18ae319d79e8511e88
annajeevan <annajeevan22@gmail.com>
2026-08-31 09:11:37 +0530

    Create 04_merge_sort.cpp

---
+++ 04_merge_sort.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+void merge(int a[], int low, int mid, int high) {
+    int temp[1000];
+    int i = low;
+    int j = mid + 1;
+    int k = 0;
+
+    while (i <= mid && j <= high) {
+        if (a[i] <= a[j])
+            temp[k++] = a[i++];
+        else
+            temp[k++] = a[j++];
+    }
+
+    while (i <= mid)
+        temp[k++] = a[i++];
+
+    while (j <= high)
+        temp[k++] = a[j++];
+
+    for (i = low, k = 0; i <= high; i++, k++)
+        a[i] = temp[k];
+}
+
+void mergeSort(int a[], int low, int high) {
+    if (low < high) {
+        int mid = (low + high) / 2;
+
+        mergeSort(a, low, mid);
+        mergeSort(a, mid + 1, high);
+
+        merge(a, low, mid, high);
+    }
+}
+
+int main() {
+    int n;
+    int a[1000];
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    mergeSort(a, 0, n - 1);
+
+    cout << "Sorted array: ";
+
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " ";
+
+    return 0;
+}
